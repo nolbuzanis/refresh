130664b8b464fcda253cc39985b7839f673e3d15
nolbuzanis <nolanbuzanis@gmail.com>
2021-05-14 09:50:28 -0400

    feat: layout will grow to full page height even if content is smaller

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import { Layout } from 'antd';
+import styled from 'styled-components';
 import Footer from './Footer';
 import Header from './Header';
 
 const { Content } = Layout;
 
+const LayoutBody = styled.div`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+
+const StyledContent = styled(Content)`
+  flex-grow: 1;
+`;
+
 const LayoutWrapper: React.FC = ({ children }) => (
-  <>
+  <LayoutBody>
     <Header />
-    <Content>{children}</Content>
+    <StyledContent>{children}</StyledContent>
     <Footer />
-  </>
+  </LayoutBody>
 );
 
 export default LayoutWrapper;
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import { PageProps } from 'gatsby';
+import styled from 'styled-components';
 import Layout from '../components/Layout';
+
+const Center = styled.div`
+  text-align: center;
+  padding-top: 200px;
+`;
 
 const NotFound: React.FC<PageProps> = () => (
   <Layout>
-    <p>Sorry, page not found!</p>
+    <Center>
+      <h1>Sorry, page not found!</h1>
+    </Center>
   </Layout>
 );
 
